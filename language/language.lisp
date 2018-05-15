0f735f7deaeee28deed486232fb20cccc85f4abf
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-05-15 23:40:22 +0300

    Split examples + fix dash in depth

--- language/language.lisp
+++ language/language.lisp
@@ -2,7 +2,9 @@
 	(let
 		((args (cdr equation)))
 		(some
-			(lambda (x) (eq x '_))
+			(lambda (x)
+				(or (eq x '_) (and (listp x) (has-dash x)))
+			)
 			args
 		)
 	)
@@ -44,26 +46,3 @@
 		val
 	)
 )
-
-;EXAMPLES
-
-(print
-	(
-		: 10
-		(+ _ 1 2)	;13
-		(- 4 _)		;-9
-		(* -1)		;9
-		(- 4)		;5
-		(* _ _)		;25
-	)
-)
-
-(print
-	(
-		: (list 1 2 3 4 5)
-		(cdr _)					;(2 3 4 5)
-		(cdr)					;(3 4 5)
-		(append (list 6 7 8) _)	;(6 7 8 1 2 3)
-	)
-)
-
